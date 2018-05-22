81313a942df4324814fec186dc13114a9d82f1dd
Lynne Jones <lynnej@uw.edu>
2018-05-22 16:31:01 +0200

    Add LCDB to catalog_utils

--- catalog_utils.py
+++ catalog_utils.py
@@ -1,6 +1,7 @@
-import os
+import numpy as np
 import pandas as pd
 
+__all__ = ['read_sdss_moc', 'read_astorb', 'read_lcdb']
 
 def _convert_designation(x):
     # Match astorb designation to MOC designation.
@@ -13,6 +14,10 @@ def read_sdss_moc(filename='ADR4.dat'):
 
     SDSS MOC available from:
     http://faculty.washington.edu/ivezic/sdssmoc/sdssmoc.html
+    Please see information on that page for citation information.
+    Ivezic, Z., Juric, M., Lupton, R.H., Tabachnik, S. & Quinn, T. (the SDSS Collaboration) 2002,
+    Survey and Other Telescope Technologies and Discoveries, J.A. Tyson, S. Wolff, Editors,
+    Proceedings of SPIE Vol. 4836 (2002)
 
     The SDSS MOC provides visible colors for moving objects, with varying numbers of
     observations per object. Estimates of the uncertainties are provided.
@@ -68,7 +73,8 @@ def read_astorb(filename='astorb.dat'):
            'obsArc', 'nObs', 'epoch', 'meanAnom', 'argPeri', 'lonNode', 'inc', 'e', 'a',
            'date_comp', 'ceu', 'deltaceu', 'ceudate',
            'peumax', 'peumaxdate', 'peumax10', 'peumax10date', 'peumax2', 'peumax2date']
-    widths = [7, 19, 16, 6, 6, 5, 6, 5, 4, 4, 4, 4, 4, 4, 6, 6, 9, 11, 11, 11, 10, 11, 13, 9, 8, 8, 9, 8, 9, 8, 9, 8, 9]
+    widths = [7, 19, 16, 6, 6, 5, 6, 5, 4, 4, 4, 4, 4, 4, 6, 6, 9, 11, 11, 11, 10, 11, 13,
+              9, 8, 8, 9, 8, 9, 8, 9, 8, 9]
     astorb = pd.read_fwf(filename, index_col=False, names=names, widths=widths)
     astorb = astorb.apply(_convert_designation, axis=1)
     astorb['numberId'] = astorb['numberId'].fillna(0).astype(int)
@@ -78,3 +84,45 @@ def read_astorb(filename='astorb.dat'):
 # ASTDYS proper elements
 # family classifications
 # http://hamilton.dm.unipi.it/astdys/index.php?pc=5
+
+def read_lcdb(filename='LC_SUM_PUB.TXT'):
+    """Read LCDB file tabulating periods and associated information.
+
+    LCDB available from:
+    http://www.minorplanet.info/lightcurvedatabase.html
+    Please see that page for citation information.
+    Warner, B.D., Harris, A.W., Pravec, P. (2009). Icarus 202, 134-146.
+    Updated <Date of last update>. http://www.MinorPlanet.info/lightcurvedatabase.html
+
+    The Asteroid Lightcurve Database is a listing of asteroid lightcurve parameters and other information,
+    e.g., estimated/measured diameters, absolute magnitudes (H), phase slope parameters (G), albedos, + more.
+
+    Parameters
+    ----------
+    filename : str, opt
+        The full path to the LCDB file (LC_SUM_PUB.TXT or similarly formatted files)
+
+    Returns
+    -------
+    pandas.DataFrame
+    """
+    names = ['Number', 'New', 'Name', 'Desig', 'Family', 'Csource', 'Class', 'Dsource', 'Dflag', 'Diameter',
+             'Hsource', 'H', 'Hband', 'Asource', 'Albedoflag',  'Albedo', 'Pflag', 'Period', 'Pdescr',
+             'Ampflag', 'AmpMin', 'AmpMax', 'U', 'Notes', 'Binary', 'Priv' , 'Pole', 'Sparse', 'Widefield']
+    colspecs = [(0, 7), (8, 9), (10, 40), (41, 61), (62, 70), (71, 72), (73, 78), (79, 80), (81, 82),
+                (83, 92), (93, 94), (95, 100), (101, 102), (104, 105), (106, 107), (108, 114),
+                (115, 116), (117, 130), (131, 146), (147, 148), (149, 153), (154, 158), (159, 161),
+                (162, 167), (168, 171), (172, 175), (176, 179), (180, 182), (183, 184)]
+    lcdata = pd.read_fwf(filename, index_col=False, skiprows=5, names=names, colspecs=colspecs)
+    lcdata['Frequency'] = 1.0/lcdata.Period
+    # Map flags to ints.
+    tmp = lcdata.New.values
+    tmp = np.where(tmp == '*', 1, 0)
+    lcdata.New = tmp
+    tmp = lcdata.Sparse.values
+    tmp = np.where(tmp == 'Y', 1, 0)
+    lcdata.Sparse = tmp
+    tmp = lcdata.Widefield.values
+    tmp = np.where(tmp == 'Y', 1, 0)
+    lcdata.Widefield = tmp
+    return lcdata
